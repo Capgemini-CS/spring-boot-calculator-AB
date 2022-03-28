cf5be2968c307998a1ca5992f6cb3fcebb23b769
AndreeaaBadea <andreea.badea@capgemini.com>
2022-03-28 10:33:43 +0300

    Setup project

---
+++ src/test/java/com/example/org/calculator/CalculatorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.org.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
